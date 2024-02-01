abe606df4b0440fe95bf4e6d165e010070b31690
MUDATHIR1 <88151040+MUDATHIR1@users.noreply.github.com>
2024-02-01 14:52:19 +0200

    ex1.php

---
+++ Mudathir_tasks/ex1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercise 1: Getting Started with PHP - Mudathir</title>
+</head>
+<body>
+  <h3>3.1 Write PHP code to output the following message:
+    Hello world! My name is "David"
+</h3>  
+<?php
+echo "Hello world! My name is \"David\"";
+?>
+</body>
+</html>
