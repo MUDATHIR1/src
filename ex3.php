5457f1679b0a09a1953fe0d3c785f8d51be14fb3
MUDATHIR1 <88151040+MUDATHIR1@users.noreply.github.com>
2024-02-06 15:19:17 +0200

    b

--- ex3.php
+++ ex3.php
@@ -1,14 +1,13 @@
 <?php
 $title = "Exercise 3: Variable, String and Operators";
-include 'header.php';?>
+include 'header.php'?>
 
-
+<h3>3.1 Create a file (ex3.php): Create a file named 
+ex3.php in your exercise folder. 
+Ensure that it uses the same layout 
+as your other pages and that your navigation 
+bar includes a link to this exercise.</h3> 
 
 
 <?php include 'footer.php'?>
 
-Create a file (ex3.php): Create a file named 
-ex3.php in your exercise folder. 
-Ensure that it uses the same layout 
-as your other pages and that your navigation 
-bar includes a link to this exercise.
