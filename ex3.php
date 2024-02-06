fcfd49b48cb8657635af2c128bad9bc64098ade3
MUDATHIR1 <88151040+MUDATHIR1@users.noreply.github.com>
2024-02-06 15:41:44 +0200

    h

--- ex3.php
+++ ex3.php
@@ -2,12 +2,21 @@
 $title = "Exercise 3: Variable, String and Operators";
 include 'header.php'?>
 
-<p><h3>3.1 Create a file (ex3.php): Create a file named 
+<h3>3.1 Create a file (ex3.php): Create a file named 
 ex3.php in your exercise folder. 
 Ensure that it uses the same layout 
 as your other pages and that your navigation 
-bar includes a link to this exercise.</h3></p> 
+bar includes a link to this exercise.</h3> 
 
+<h3>Form Creation: Create a simple HTML form to 
+collect the user’s Firstname and Lastname.
+ Use the echo statement to print “Hello [Firstname] [Lastname],
+' You are welcome to my site.” inside an h3</h3>
+
+<form action="">
+Fname: <input type="text" Fname = "Fisrtname" required> <br><br>
+Lname: <input type="text" Lname= "Lastname" required> <br><br>
+</form>
 
 <?php include 'footer.php'?>
 
