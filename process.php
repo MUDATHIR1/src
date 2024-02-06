ba033494ef530c7349e7e54ad57e4fcead0eea75
MUDATHIR1 <88151040+MUDATHIR1@users.noreply.github.com>
2024-02-06 17:03:07 +0200

    h

--- process.php
+++ process.php
@@ -5,7 +5,7 @@ include 'header.php';
 $Firstname = $_POST["Firstname"];
 $Lastname = $_POST["Lastname"];
 
-echo "Hello: " . $Firstname . " " . $Lastname . "<br> You are welcome to my site.";
+echo "Hello: " . $Firstname . " , " . $Lastname . " <br> You are welcome to my site.";
 
 include 'footer.php';
 ?>
