b8d43f876e7f2c0f050e3539c111b38e56bc28e9
MUDATHIR1 <88151040+MUDATHIR1@users.noreply.github.com>
2024-02-06 16:27:34 +0200

    ex3

--- process.php
+++ process.php
@@ -1,9 +1,11 @@
 <?php
 //what to do with the data collected in the form
-
+<?php include 'header.php'?>
 
 $Fname = $_post["Fname"];
 $Fname = $_post["Lname"];
 
-echo "Hello " . $Fname, $Lname, . " You are welcome to my site.";
+echo "Hello " . $Fname, $Lname, <br>" You are welcome to my site.";
+
+<?php include 'footer.php'?>
 ?>
