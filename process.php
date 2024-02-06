18e74f018156c554183dfe2472219fbc5bacc615
MUDATHIR1 <88151040+MUDATHIR1@users.noreply.github.com>
2024-02-06 16:14:55 +0200

    ex3

--- process.php
+++ process.php
@@ -1,4 +1,9 @@
 <?php
 //what to do with the data collected in the form
 
+
+$Fname = $_post["Fname"];
+$Fname = $_post["Lname"];
+
+echo "Hello " . $Fname $Lname, " You are welcome to my site."
 ?>
