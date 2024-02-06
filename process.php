7cd18591e9a0f721402fda580f5b7d0a83ff732e
MUDATHIR1 <88151040+MUDATHIR1@users.noreply.github.com>
2024-02-06 16:48:54 +0200

    b

--- process.php
+++ process.php
@@ -2,10 +2,9 @@
 //what to do with the data collected in the form
 include 'header.php';
 
-$Firstname = $_POST["Firstname"];
-$Lastname = $_POST["Lastname"];
+$Result = $_POST["Firstname", "Lastname"];
 
-echo "Hello: " . $Firstname, $Lastname "<br> You are welcome to my site.";
+echo "Hello: " . $Result . " <br> You are welcome to my site.";
 
 include 'footer.php';
 ?>
