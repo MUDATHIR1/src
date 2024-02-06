0521256663a75348571055c60d769b72b76a52e0
MUDATHIR1 <88151040+MUDATHIR1@users.noreply.github.com>
2024-02-06 15:00:21 +0200

    ju

---
+++ new.php
@@ -0,0 +1,4 @@
+<?php include 'header.php'?>
+<h2>Hello world this is a new page </h2>
+<p>Define three variables: $g1 = 5, $g2 = 4, and $g3 = 5. These variables represent the grades of three students in the course. To display this information, create an HTML table within your PHP code. The table should be structured with columns for Serial Number (S.n.), Name, and Grade, and it should look like this: </p>
+<?php include 'footer.php'?>
